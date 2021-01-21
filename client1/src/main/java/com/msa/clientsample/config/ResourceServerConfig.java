2ac2459acd98ec05a9d3770c622fb7a5f14d60e5
n1tjrgns <n2tjrgns@gmail.com>
2021-01-21 16:56:28 +0900

    OauthResourceServer 모듈 삭제 게이트웨이 ResourceServerConfig로 이전

--- client1/src/main/java/com/msa/clientsample/config/ResourceServerConfig.java
+++ client1/src/main/java/com/msa/clientsample/config/ResourceServerConfig.java
@@ -1,4 +1,4 @@
-package com.msa.oauthconfig.config;
+package com.msa.clientsample.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,13 +7,13 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 
 @EnableResourceServer
 @Configuration
-public class Oauth2ServerConfig extends ResourceServerConfigurerAdapter {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.headers().frameOptions().disable();
         http.authorizeRequests()    // '/' url 접근시 read 스코프가 존재해야 접근 가능하도록 설정
-                .antMatchers("/").access("#oauth2.hasScope('read')")
+                .antMatchers("/ping").access("#oauth2.hasScope('read')")
                 .anyRequest().authenticated();
     }
 }
